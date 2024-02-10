9e2198ee1fdb95fb4353b14df5ca808af5b00d2e
Shubham Kadam <kadamshubham098@gmail.com>
2024-02-10 20:03:25 +0530

    Added Navbar component

---
+++ Frontend/src/components/Header/Navbar.tsx
@@ -0,0 +1,118 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      !(dropdownRef.current as any).contains(event.target)
+    ) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-primary-color border-b border-solid dark:border-border-color relative min-h-18">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <svg
+            width="2rem"
+            height="2rem"
+            viewBox="0 0 76 65"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#ffffff" />
+          </svg>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-primary-font-color">
+            Vercel
+          </span>
+        </a>
+        <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative">
+          <button
+            type="button"
+            className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button"
+            aria-expanded={isDropdownOpen}
+            onClick={toggleDropdown}
+            ref={dropdownRef}
+          >
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://a0.anyrgb.com/pngimg/1070/974/file-icon-icons-8-user-profile-internet-forum-online-chat-avatar-flat-businessperson-public-relations-conversation.png"
+              alt="user photo"
+            />
+          </button>
+          {/* <!-- Dropdown menu --> */}
+          <div
+            className={`z-50 ${
+              isDropdownOpen ? "" : "hidden"
+            } my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:divide-gray-600 dark:border dark:border-solid dark:border-border-color absolute top-6 right-0 dark:bg-card-color`}
+            id="user-dropdown"
+            ref={dropdownRef}
+          >
+            <div className="px-4 py-3">
+              <span className="block text-sm text-gray-900 dark:text-white">
+                Bonnie Green
+              </span>
+              <span className="block text-sm text-white truncate dark:text-gray-400">
+                name@flowbite.com {/* Changed text color to white */}
+              </span>
+            </div>
+            <ul className="py-2" aria-labelledby="user-menu-button">
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Earnings
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
