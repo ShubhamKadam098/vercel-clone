e3d809c40f1e92eb8942648515f3bc322a78e387
Shubham Kadam <kadamshubham098@gmail.com>
2024-02-15 11:45:13 +0530

    Added react router config and routes

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App";
+import SignUp from "./components/SignUp";
+import PublicLayout from "./components/Layout/PublicLayout";
+import PageNotFound from "./components/Dummy/PageNotFound";
+import SignIn from "./components/SignIn";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<PublicLayout />}>
+        <Route path="" element={<App />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+      </Route>
+      <Route path="*" element={<PageNotFound />}></Route>
+    </>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
