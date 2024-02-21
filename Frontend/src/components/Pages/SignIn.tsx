4824274ea1ce0831887c2e5ff973abd64379292b
Shubham Kadam <kadamshubham098@gmail.com>
2024-02-21 22:58:43 +0530

    Added login functionality.

--- Frontend/src/components/Pages/SignIn.tsx
+++ Frontend/src/components/Pages/SignIn.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import {
+  doSignInWithEmailAndPassword,
+  doSignInWithGoogle,
+} from "../../firebase/auth";
+import { AuthError } from "firebase/auth";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      toast.error("All fields are required");
+      return;
+    }
+    try {
+      await doSignInWithEmailAndPassword(email, password);
+      toast.success("Signed in successfully.");
+    } catch (error: unknown) {
+      console.error("Error in Signning in with email and password: ", error);
+      const authError = error as AuthError;
+      if (authError?.message) {
+        toast.error(authError.message);
+      } else toast.error("Invalid email or password. Please try again.");
+    }
+  };
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      await doSignInWithGoogle();
+      toast.success("Signed in successfully with Google.");
+    } catch (error: unknown) {
+      console.error("Error while signing with google: ", error);
+      const authError = error as AuthError;
+      if (authError?.message) {
+        toast.error(authError.message);
+      }
+      toast.error("Error signing in with Google. Please try again.");
+    }
+  };
   return (
     <section className="bg-gray-50 dark:bg-primary-color">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -22,6 +62,8 @@ const SignIn = () => {
                   type="email"
                   name="email"
                   id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required
@@ -35,9 +77,13 @@ const SignIn = () => {
                   Password
                 </label>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   id="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                   required
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -47,15 +93,19 @@ const SignIn = () => {
                 <div className="flex items-start">
                   <div className="flex items-center h-5">
                     <input
-                      id="remember"
-                      aria-describedby="remember"
+                      id="showPassword"
+                      aria-describedby="showPassword"
                       type="checkbox"
+                      checked={showPassword}
+                      onChange={() => {
+                        setShowPassword(!showPassword);
+                      }}
                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
                     />
                   </div>
                   <div className="ml-3 text-sm">
                     <label
-                      htmlFor="remember"
+                      htmlFor="showPassword"
                       className="text-gray-500 dark:text-gray-300"
                     >
                       Show Password
@@ -69,8 +119,45 @@ const SignIn = () => {
                   Forgot password?
                 </Link>
               </div>
-              <button className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <button
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
                 Sign in
+              </button>
+              <p className="mt-4 text-center text-gray-600 dark:text-gray-400">
+                or sign in with
+              </p>
+
+              <button
+                type="button"
+                className="flex w-full items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg  bg-primary-font-color hover:bg-gray-50 "
+                onClick={() => {
+                  handleSignInWithGoogle();
+                }}
+              >
+                <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
+                  <path
+                    d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                    fill="#FFC107"
+                  />
+                  <path
+                    d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                    fill="#FF3D00"
+                  />
+                  <path
+                    d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                    fill="#4CAF50"
+                  />
+                  <path
+                    d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                    fill="#1976D2"
+                  />
+                </svg>
+
+                <span className="mx-2">Sign in with Google</span>
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
