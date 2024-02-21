effe0f514314f339ee6d62b0b05e94b4491713b6
Shubham Kadam <kadamshubham098@gmail.com>
2024-02-21 23:37:56 +0530

    Implemented signup functionality

--- Frontend/src/components/Pages/SignUp.tsx
+++ Frontend/src/components/Pages/SignUp.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { FormEvent, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { doCreateUserWithEmailAndPassword } from "../../firebase/auth";
+import { AuthError } from "firebase/auth";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [isTermsChecked, setIsTermsChecked] = useState(false);
+  const [isMatchingPassword, setIsMatchingPassword] = useState(true);
+
+  useEffect(() => {
+    setIsMatchingPassword(confPassword === password);
+  }, [confPassword, password]);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!isTermsChecked) {
+      toast.error("Please accept our Terms and Conditions to proceed");
+      return;
+    }
+
+    if (!isMatchingPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+
+    if (!email || !password || !confPassword) {
+      toast.error("All fields are required");
+      return;
+    }
+
+    try {
+      await doCreateUserWithEmailAndPassword(email, password);
+      toast.success("User created successfully");
+    } catch (error: unknown) {
+      console.error("Error creating user:", error);
+      const authError = error as AuthError;
+      if (authError?.message) {
+        toast.error(authError.message);
+      } else toast.error("Error creating user. Please try again.");
+    }
+  };
+
   return (
     <section className="bg-gray-50 dark:bg-primary-color">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -10,7 +53,7 @@ const SignUp = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create Your Vercel Account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="email"
@@ -23,6 +66,8 @@ const SignUp = () => {
                   name="email"
                   id="email"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                 />
@@ -40,6 +85,8 @@ const SignUp = () => {
                   id="password"
                   placeholder="••••••••"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
@@ -51,13 +98,24 @@ const SignUp = () => {
                   Confirm password
                 </label>
                 <input
-                  type="confirm-password"
+                  type="password"
                   name="confirm-password"
                   id="confirm-password"
                   placeholder="••••••••"
                   required
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={confPassword}
+                  onChange={(e) => setConfPassword(e.target.value)}
+                  className={
+                    isMatchingPassword
+                      ? `bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`
+                      : "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+                  }
                 />
+                {!isMatchingPassword && (
+                  <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    Passwords do not match
+                  </p>
+                )}
               </div>
               <div className="flex items-start">
                 <div className="flex items-center h-5">
@@ -66,6 +124,8 @@ const SignUp = () => {
                     aria-describedby="terms"
                     type="checkbox"
                     required
+                    checked={isTermsChecked}
+                    onChange={() => setIsTermsChecked(!isTermsChecked)}
                     className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
                   />
                 </div>
@@ -93,7 +153,7 @@ const SignUp = () => {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
                 <Link
-                  to="/signin"
+                  to="/login"
                   className="font-medium text-blue-600 hover:underline dark:text-blue-500"
                 >
                   Login here
